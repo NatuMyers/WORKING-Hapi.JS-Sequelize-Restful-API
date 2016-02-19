131c464c2339688821450017ce2f4eb25e799b00
Natu Myers <me@natumyers.com>
2016-02-19 00:16:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var Hapi = require('hapi');
+var models = require('./models');
+
+
+// create the server
+var server = new Hapi.Server();
+server.connection({ port : 3000 })
+
+// routes
+server.route(require('./lib/routes'))
+
+
+models.sequelize.sync().then(function() {
+  server.start(function() {
+    console.log('Running on 3000');
+  });
+});
