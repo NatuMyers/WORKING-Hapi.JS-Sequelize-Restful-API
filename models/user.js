ffe91ee952a3faaf2726b013e9e199ddca9de664
Natu Myers <me@natumyers.com>
2016-02-20 20:59:53 -0500

    Added files via upload

--- models/user.js
+++ models/user.js
@@ -1,7 +1,32 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING
+    username: DataTypes.STRING,
+    
+    userType: DataTypes.STRING,
+
+	email: DataTypes.STRING,
+	k: DataTypes.STRING,
+	fname: DataTypes.STRING,
+	lname: DataTypes.STRING,	
+	username: DataTypes.STRING,
+	summary: DataTypes.STRING,
+	
+	messagesSent: DataTypes.INTEGER,
+	numCollabs: DataTypes.INTEGER,
+	successfulCollabs: DataTypes.INTEGER,
+	followers: DataTypes.INTEGER,
+	following: DataTypes.INTEGER,
+	rating: DataTypes.INTEGER,
+	
+	isShowingSoc: DataTypes.BOOLEAN,
+	isShowingLoc: DataTypes.BOOLEAN,
+	isShowingSO: DataTypes.BOOLEAN,
+	isSuspended: DataTypes.BOOLEAN,
+	isBanned: DataTypes.BOOLEAN,
+	isAuth: DataTypes.BOOLEAN,
+	dob: DataTypes.BOOLEAN
+
   }, {
     classMethods: {
       associate: function(models) {
